c313a6d3b1e7429b2eea06975e980c4043b66b27
Rescobarmo <rescobarmo@redsalud.cl>
2026-08-19 16:52:02 -0400

    Fix: prioridad a env vars de EasyPanel sobre archivo .env

--- config/database.php
+++ config/database.php
@@ -8,7 +8,9 @@ if (file_exists($envFile)) {
             [$key, $value] = explode('=', $line, 2);
             $key = trim($key);
             $value = trim($value);
-            putenv("$key=$value");
+            if (getenv($key) === false) {
+                putenv("$key=$value");
+            }
         }
     }
 }
